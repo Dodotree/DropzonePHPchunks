5f9ec13d8b33b380c0c9c3de1775d5de26380ddf
Olga, Paul & Veny T <tchistopolskii@gmail.com>
2018-06-01 21:30:38 -0700

    Create chunks.js

---
+++ chunks.js
@@ -0,0 +1,20 @@
+new Dropzone(id,{
+            url: 'chunks.php',
+            chunking: true,
+            chunkSize: 1*1024*1024,
+            forceChunking: false,
+            parallelChunkUploads: true,
+            parallelChunkUploads: true,
+            retryChunks: true,
+            retryChunksLimit: 3,
+            chunksUploaded: function(big_file, done_func){
+                done_func();
+            },
+            sending: function(a,b,formdata){ // in case you want to add data and not override chunk info
+                $.each(params, function(nm,vl){ 
+                    formdata.append(nm,vl);
+                });
+            },
+            dictDefaultMessage: message,
+            paramName: paramName
+        });
